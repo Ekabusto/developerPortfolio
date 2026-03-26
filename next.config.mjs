1561c23d9abfaa14c356c0d85401b8489aa756c7
ekaitzbustork <151842435+ekaitzbustork@users.noreply.github.com>
2026-03-26 16:42:23 +0100

    CI CD fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-  // This ensures that the static export works even when deployed to a subdirectory
-  basePath: '',
-  // Disable server-side features that aren't compatible with static exports
-  trailingSlash: true,
 }
 
 export default nextConfig
